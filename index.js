0c609e4c068e4e74b52b7f671ba19c146f353ec0
Fitzz <wenjingbiaoi@qq.com>
2023-08-18 17:33:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,100 +12,207 @@ function stringifyRequest(loader, url) {
 	if (IS_MODULE_REQUEST.test(url)) url = url.replace(IS_MODULE_REQUEST, "");
 	return contextify(url);
 }
-
 function loader(css) {
-	try {
-		const replacements = getReplacements(css);
-		let code = JSON.stringify(css);
-		const beforeCode = `
-		var ___CSS_LOADER_EXPORT___ = [];
-		export default ___CSS_LOADER_EXPORT___;	`.trim();
-		let importCodes = "";
-		for (const [replacementName, importName] of replacements.entries()) {
-			let url = stringifyRequest(this, replacementName);
-			importCodes += `var  ${importName} = new URL(${url}, import.meta.url);\n`;
-			code = code.replace(
-				new RegExp(replacementName, "g"),
-				() => `" + ${importName} + "`
-			);
-		}
-		// Indexes description:
-		// 0 - module id
-		// 1 - CSS code
-		return `${beforeCode}//Imports \n${importCodes}// Module\n___CSS_LOADER_EXPORT___.push([module.id, ${code}]);\n`;
-	} catch (error) {
-		throw error;
+	const urls = urlParser(css);
+	let code = JSON.stringify(css);
+	const beforeCode = `var CSS_CHUNK = [];export default CSS_CHUNK;`.trim();
+	let importCodes = "";
+	let index = 0;
+	for (let [startPos, endPos] of urls) {
+		const urlValue = css.slice(startPos, endPos);
+		const importName = `___CSS_LOADER_URL_IMPORT_${index}`;
+		const importUrl = stringifyRequest(this, urlValue);
+		importCodes += `var  ${importName} = new URL(${importUrl}, import.meta.url);\n`;
+		code = code.replace(
+			new RegExp(urlValue, "g"),
+			() => `" + ${importName} + "`
+		);
+		index++;
 	}
+	// Indexes description:
+	// 0 - module id
+	// 1 - CSS code
+	return `${beforeCode}//Imports\n${importCodes}// Module\nCSS_CHUNK.push([module.id, ${code}]);\n`;
 }
 
-function getReplacements(source) {
-	const replacements = new Map();
+function urlParser(source) {
+	const urlsPositions = new Set();
 	let pos = -1;
-	let end = source.length - 1;
-	let ch = 0;
-	source = source.trim();
-
+	let max = source.length;
 	// 34 ""
 	// 39 '
 	// 105 i
 	// 45 -
 	// 117 u
+	// 40 (
+	// 41 )
 
-	while (pos++ < end) {
-		ch = source.charCodeAt(pos);
-		if (ch === 32 || (ch < 14 && ch > 8)) continue;
-		// url
-		if (ch == 117) {
-			if (source.startsWith("rl", pos + 1)) {
-				pos++;
-				tryParseUrlFunction();
-			}
-		}
-		// 45 -   -webkit-image-set
-		// 105 i  image-set
-		if (ch == 105) {
-			let isImageSet = source.slice(pos, pos + 9) == "image-set";
-			if (isImageSet) tryParseImageSet();
-		}
-	}
-	function tryParseImageSet() {
-		pos += 10;
-		let ch = 0;
-		while (pos++ < end) {
-			ch = source.charCodeAt(pos);
-			if (ch == 32 || (ch > 8 && ch < 14)) continue;
-			// image-set(url("url/to/file"))
-			// -webkit-image-set(url("url/to/file"))
-			if (ch == 117 && source.slice(pos, pos + 3) == "url") {
-				pos++;
-				tryParseUrlFunction();
-			}
-
-			//  image-set("url/to/file")
-			//  -webkit-image-set("url/to/file")
-			else if (ch == 34 || ch == 39) pos++;
-		}
-	}
-
-	function tryParseUrlFunction() {
-		pos += 3;
-		let ch = source.charCodeAt(pos);
-		if (ch == 34 || ch == 39) pos++;
-		let endPos = -1;
+	function tryParseUrl() {
+		pos += 4;
 		let startPos = pos;
-		while (pos++ < end) {
-			let ch = source.charCodeAt(pos);
-			if (ch == 34 || ch == 39) continue;
-			if (startPos == -1) startPos = pos;
-			if (ch != 41) endPos = pos + 1;
-			else break;
+		let ch = source.charCodeAt(pos);
+		if (ch == 34 || ch == 39) {
+			pos++;
+			startPos = pos;
 		}
-		let replacementKey = source.slice(startPos, endPos);
-		let replacementName = replacements.get(replacementKey);
-		if (!replacementName) {
-			replacementName = `___CSS_LOADER_URL_IMPORT_${replacements.size}`;
-			replacements.set(replacementKey, replacementName);
+		while (pos++ < max) {
+			ch = source.charCodeAt(pos);
+			if (ch === 32 || (ch < 14 && ch > 8)) continue;
+			// ")
+			if (ch == 34 || ch == 39) ch = source.charCodeAt(pos + 1);
+			if (ch == 41) {
+				urlsPositions.add([startPos, pos]);
+				break;
+			}
 		}
 	}
-	return replacements;
+	function tryParseQuoteUrl() {
+		pos++;
+		let startPos = pos;
+		let ch = commentWhitespace(true);
+		while (pos++ < max) {
+			ch = source.charCodeAt(pos);
+			if (ch != 34 && ch != 39) continue;
+			let next_ch = source.charCodeAt(pos + 1);
+			urlsPositions.add([startPos, pos]);
+			/*
+			image-set("url/to/path/1","url/to/path/2")
+			image - set(
+				"url/to/path/1",
+				"url/to/path/2"
+			)
+			 */
+			if (next_ch == 44) {
+				pos++;
+				if (ch == 34) stringLiteral(ch);
+				ch = commentWhitespace(true);
+				tryParseQuoteUrl();
+			} else if (next_ch == 32) {
+				/*
+			image - set(
+				"url/to/path/1"  type(mime-type),
+				"url/to/path/2"  type(mime-type)
+			)
+			 */
+				ch = commentWhitespace(true);
+				const stash = pos;
+				while (pos++ < max) {
+					let ch = source.charCodeAt(pos);
+					if (ch == 32 || (ch < 14 && ch > 8)) continue;
+					if (ch == 116 && source.slice(pos, pos + 5) == "type(") {
+						pos += 5;
+						ch = commentWhitespace(true);
+						stringLiteral(ch);
+						if (ch == 34 || ch == 39) {
+							pos += 1;
+							ch = commentWhitespace(true);
+							// )
+							if (ch == 41) pos += 1;
+							// ),
+							ch = source.charCodeAt(pos);
+							if (ch == 44) {
+								pos += 1;
+								commentWhitespace(true);
+								tryParseQuoteUrl();
+							}
+							break;
+						}
+					}
+				}
+				pos = stash;
+			}
+
+			break;
+		}
+	}
+
+	function mainParser() {
+		while (pos++ < max) {
+			let ch = source.charCodeAt(pos);
+			if (ch === 32 || (ch < 14 && ch > 8)) continue;
+
+			// 117 - u  url()
+			if (ch == 117) {
+				let atImportRule = `@import `;
+				if (source.slice(pos, pos + 4) == "url(") {
+					let part = source.slice(pos - atImportRule.length - 1, pos).trim();
+					if (part == "@import") continue;
+					tryParseUrl();
+				}
+			}
+			//  105 -> i
+			//  image-set()
+			//  -webkit-image-set()
+			if (ch == 105) {
+				const isImageSet = source.slice(pos, pos + 10) == "image-set(";
+				if (isImageSet) {
+					pos += 10;
+					ch = commentWhitespace(true);
+					//  image-set(url(url/to/image))
+					if (ch == 117) tryParseUrl();
+					//  image-set("url/to/image")
+					if (ch == 34 || ch == 39) tryParseQuoteUrl();
+				}
+			}
+		}
+		return urlsPositions;
+	}
+	return mainParser();
+	// Ported from es-module-lexer
+	function isBr(c) {
+		return c === 13 /*\r*/ || c === 10 /*\n*/;
+	}
+
+	function isWsNotBr(c) {
+		return c === 9 || c === 11 || c === 12 || c === 32 || c === 160;
+	}
+
+	function isBrOrWs(c) {
+		return (c > 8 && c < 14) || c === 32 || c === 160;
+	}
+	function commentWhitespace(br) {
+		let ch;
+		do {
+			ch = source.charCodeAt(pos);
+			if (ch === 47 /*/*/) {
+				const next_ch = source.charCodeAt(pos + 1);
+				if (next_ch === 47 /*/*/) lineComment();
+				else if (next_ch === 42 /***/) blockComment(br);
+				else return ch;
+			} else if (br ? !isBrOrWs(ch) : !isWsNotBr(ch)) {
+				return ch;
+			}
+		} while (pos++ < max);
+		return ch;
+	}
+
+	function blockComment(br) {
+		pos++;
+		while (pos++ < max) {
+			const ch = source.charCodeAt(pos);
+			if (!br && isBr(ch)) return;
+			if (ch === 42 /***/ && source.charCodeAt(pos + 1) === 47 /*/*/) {
+				pos++;
+				return;
+			}
+		}
+	}
+
+	function lineComment() {
+		while (pos++ < max) {
+			const ch = source.charCodeAt(pos);
+			if (ch === 10 /*\n*/ || ch === 13 /*\r*/) return;
+		}
+	}
+
+	function stringLiteral(quote) {
+		while (pos++ < max) {
+			let ch = source.charCodeAt(pos);
+			if (ch === quote) return;
+			if (ch === 92 /*\*/) {
+				ch = source.charCodeAt(++pos);
+				if (ch === 13 /*\r*/ && source.charCodeAt(pos + 1) === 10 /*\n*/) pos++;
+			} else if (isBr(ch)) break;
+		}
+	}
 }
